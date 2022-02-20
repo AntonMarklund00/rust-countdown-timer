fe3d8fd8cc4ad860aa811cd6b70244b3714a5c6c
Anton Marklund <anton.marklund00@gmail.com>
2022-02-20 23:17:17 +0100

    correct calculation

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,32 @@ use std::{io, time, thread, fs};
 
 fn main() {
 
-    let mut start_time = get_start_time();
-    
-    while start_time != 0{
+    println!("Please fill in amount of hours!");
+    let start_time_hours = get_start_time();
+    println!("Please fill in amount of minutes!");
+    let start_time_minutes= get_start_time();
+    println!("Please fill in amount of seconds!");
+    let start_time_seconds= get_start_time();
+
+    let mut time_in_seconds = convert_time_to_seconds(start_time_hours, start_time_minutes, start_time_seconds);
+
+    while time_in_seconds != 0{
         clear_screen();
-        print_time_left(start_time);
+        print_time_left(time_in_seconds);
         thread::sleep(time::Duration::from_secs(1));
-        start_time = start_time-1;
+        time_in_seconds = time_in_seconds-1;
     }
     
+}
+
+fn convert_time_to_seconds(hours: i32, minutes: i32, seconds: i32) -> i32{
+    hours*3600 + minutes*60 + seconds
 }
 
 fn get_start_time() -> i32{
     let start_time: i32; 
     let mut input_value = String::new();
-    println!("{}", "Please enter the countdown start time");
+
     io::stdin()
         .read_line(&mut input_value)
         .expect("Failed to read line");
@@ -34,10 +45,22 @@ fn clear_screen(){
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
 }
 
-fn print_time_left(time: i32){
+fn print_time_left(mut time: i32){
     let mut time_string_vector: Vec<String> = Vec::new();
 
-    for digit in time.to_string().split("") {
+    let hours = time / 3600;
+    println!("{}", hours);
+    time = (time - (hours * 3600));
+    println!("time left: {}", time);
+    let minutes = time / 60;
+    println!("{}", minutes);
+    time = time - (minutes / 60);
+    let seconds = time % 60;
+    println!("{}", seconds);
+
+    let time_string = format!("{:02}:{:02}:{:02}", hours, minutes, seconds);
+
+    for digit in time_string.to_string().split("") {
         if digit == "" { continue }
 
         let big_digit = get_time_in_big_ascii(&digit);
@@ -54,7 +77,8 @@ fn print_time_left(time: i32){
     print_string_vector(time_string_vector);
 }
 
-fn get_time_in_big_ascii(filename : &str) -> String{
+fn get_time_in_big_ascii(mut filename : &str) -> String{
+    if filename == ":" { filename = "seperate"}
     fs::read_to_string("digits/".to_string() + &*filename.to_string() + ".txt")
         .expect("Failed to read file")
 }
